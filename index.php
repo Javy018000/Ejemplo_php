58788fa51352ab303918b995bfbaf3d778abe4df
Javy018000 <81122338+Javy018000@users.noreply.github.com>
2022-10-20 07:18:42 -0500

    Cambio de proyecto

--- index.php
+++ index.php
@@ -1,187 +1,26 @@
-<?php 
-    include('config/constants.php');
-?>
-
+<!DOCTYPE html>
 <html>
+<head>
+	<meta charset="utf-8">	<!--Idioma español-->
+	<link rel="icon" type="icon/png" href="https://vignette.wikia.nocookie.net/es.pokemon/images/2/2c/Pok%C3%A9mon_Jukebox_icono.png/revision/latest?cb=20150624132304">
+	<link rel="stylesheet" type="text/css" href="index.css">
+	<title>Mi Primera Pagina Web</title>
+</head>
+<body>
 
-    <head>
-        <title>Task Manager with PHP and MySQL</title>
-        <link rel="stylesheet" href="<?php echo SITEURL; ?>css/style.css" />
-    </head>
-    
-    <body>
-    
-    <div class="wrapper">
-    
-    <h1>TASK MANAGER</h1>
-    
-    
-    <!-- Menu Starts Here -->
-    <div class="menu">
-    
-        <a href="<?php echo SITEURL; ?>">Home</a>
-        
-        <?php 
-            
-            //Comment Displaying Lists From Database in ourMenu
-            $conn2 = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error());
-            
-            //SELECT DATABASE
-            $db_select2 = mysqli_select_db($conn2, DB_NAME) or die(mysqli_error());
-            
-            //Query to Get the Lists from database
-            $sql2 = "SELECT * FROM tbl_lists";
-            
-            //Execute Query
-            $res2 = mysqli_query($conn2, $sql2);
-            
-            //CHeck whether the query executed or not
-            if($res2==true)
-            {
-                //Display the lists in menu
-                while($row2=mysqli_fetch_assoc($res2))
-                {
-                    $list_id = $row2['list_id'];
-                    $list_name = $row2['list_name'];
-                    ?>
-                    
-                    <a href="<?php echo SITEURL; ?>list-task.php?list_id=<?php echo $list_id; ?>"><?php echo $list_name; ?></a>
-                    
-                    <?php
-                    
-                }
-            }
-            
-        ?>
-        
-        
-        
-        <a href="<?php echo SITEURL; ?>manage-list.php">Manage Lists</a>
-    </div>
-    <!-- Menu Ends Here -->
-    
-    <!-- Tasks Starts Here -->
-    
-    <p>
-        <?php 
-        
-            if(isset($_SESSION['add']))
-            {
-                echo $_SESSION['add'];
-                unset($_SESSION['add']);
-            }
-            
-            if(isset($_SESSION['delete']))
-            {
-                echo $_SESSION['delete'];
-                unset($_SESSION['delete']);
-            }
-            
-            if(isset($_SESSION['update']))
-            {
-                echo $_SESSION['update'];
-                unset($_SESSION['update']);
-            }
-            
-            
-            if(isset($_SESSION['delete_fail']))
-            {
-                echo $_SESSION['delete_fail'];
-                unset($_SESSION['delete_fail']);
-            }
-        
-        ?>
-    </p>
-    
-    <div class="all-tasks">
-        
-        <a class="btn-primary" href="<?php SITEURL; ?>add-task.php">Add Task</a>
-        
-        <table class="tbl-full">
-        
-            <tr>
-                <th>S.N.</th>
-                <th>Task Name</th>
-                <th>Priority</th>
-                <th>Deadline</th>
-                <th>Actions</th>
-            </tr>
-            
-            <?php 
-            
-                //Connect Database
-                $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error());
-                
-                //Select Database
-                $db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error());
-                
-                //Create SQL Query to Get DAta from Databse
-                $sql = "SELECT * FROM tbl_tasks";
-                
-                //Execute Query
-                $res = mysqli_query($conn, $sql);
-                
-                //CHeck whether the query execueted o rnot
-                if($res==true)
-                {
-                    //DIsplay the Tasks from DAtabase
-                    //Dount the Tasks on Database first
-                    $count_rows = mysqli_num_rows($res);
-                    
-                    //Create Serial Number Variable
-                    $sn=1;
-                    
-                    //Check whether there is task on database or not
-                    if($count_rows>0)
-                    {
-                        //Data is in Database
-                        while($row=mysqli_fetch_assoc($res))
-                        {
-                            $task_id = $row['task_id'];
-                            $task_name = $row['task_name'];
-                            $priority = $row['priority'];
-                            $deadline = $row['deadline'];
-                            ?>
-                            
-                            <tr>
-                                <td><?php echo $sn++; ?>. </td>
-                                <td><?php echo $task_name; ?></td>
-                                <td><?php echo $priority; ?></td>
-                                <td><?php echo $deadline; ?></td>
-                                <td>
-                                    <a href="<?php echo SITEURL; ?>update-task.php?task_id=<?php echo $task_id; ?>">Update </a>
-                                    
-                                    <a href="<?php echo SITEURL; ?>delete-task.php?task_id=<?php echo $task_id; ?>">Delete</a>
-                                
-                                </td>
-                            </tr>
-                            
-                            <?php
-                        }
-                    }
-                    else
-                    {
-                        //No data in Database
-                        ?>
-                        
-                        <tr>
-                            <td colspan="5">No Task Added Yet.</td>
-                        </tr>
-                        
-                        <?php
-                    }
-                }
-            
-            ?>
-            
-            
-        
-        </table>
-    
-    </div>
-    
-    <!-- Tasks Ends Here -->
-    </div>
-    </body>
+	<!--Barra de arriba-->
+	<nav id="Nav">
+		<h1>Bienvenidos</h1>
+	</nav>
 
+	<h1 class="Titulo">¿Que es la Programacion web?</h1>
+
+	<div class="Conte">
+		<p>
+			La programación de los sitios web es una de las disciplinas dentro del mundo de Internet que más se ha desarrollado y no deja de sorprender día a día con las posibilidades que abre y genera, ya que no sólo consigue satisfacer necesidades que se generan, sino que sin la generación de necesidades ofrecen servicios a los usuarios que éstos no habían imaginado.<br><br>
+
+			En principio, el gran desarrollo de Internet se fundamentó en la posibilidad de enlazar a través de hipervículos diferentes páginas web lo que generó la enorme interconexión que es hoy Internet. Esta base que inició esta red, fué desarrollada y generó el lenguaje ícono de las páginas web que es HTML.
+		</p>
+	</div>
+</body>
 </html>
